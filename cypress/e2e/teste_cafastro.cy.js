704c3bf1bfdd7f57d42769826f1ee1661875f2f7
Luiz Arthur <luizarthursm@gmail.com>
2025-03-18 10:15:07 -0300

    Segundo update

---
+++ cypress/e2e/teste_cafastro.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Luiz');
+    cy.get('[data-test="input-email"]').type('luiz@example.com');
+    cy.get('[data-test="input-password"]').type('Teste123456');
+    cy.get('[data-test="input-confirm-password"]').type('Teste123456');
+    cy.get('[data-test="submit-button"]').click();
+  })
+})
